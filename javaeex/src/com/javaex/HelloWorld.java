1bb00b6cd96fbc83d21bf75f0fa101106a799e5e
안지훈 <tw224@naver.com>
2021-02-25 14:59:27 +0900

    initial commit

---
+++ javaeex/src/com/javaex/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.javaex;
+//는 주석이다 컴파일되지않음
+public class HelloWorld {
+/*
+ * 가나다라마바사
+ */
+	
+	//객체가 실행되고자 한다면 main 메서드 필요함
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+	
+}
---
+++ javaeex/src/com/javaex/helloworlddemo.java
@@ -0,0 +1,9 @@
+package com.javaex;
+
+public class helloworlddemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
