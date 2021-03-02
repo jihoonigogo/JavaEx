3c885ee1263d212e1752a813f5b14b2af84a2e08
안지훈 <tw224@naver.com>
2021-03-02 11:50:00 +0900

    조건문 연습

---
+++ javaeex/src/com/javaex/basic/ConditionalEx.java
@@ -0,0 +1,335 @@
+package com.javaex.basic;
+
+import java.util.Scanner;
+public class ConditionalEx {
+
+	public static void main(String[] args) {
+
+		//ifEx();
+		//ifElse();
+		//ifif();
+		//ifPractice();
+		//switchCase();
+		//switchEx2();
+		//switchEx3();
+		aaa();
+		//ifEx4();
+	}
+
+	/////////////////////////////////////////////////////////
+  private static void ifEx() { // 1. 메서드 ifex 
+	  
+	  Scanner scanner = new Scanner (System.in); //스캐너 불러옴
+	  System.out.println("점수를 입력하시오"); 
+	  int score = scanner.nextInt(); //스캐너를 이용한 정수값 입력 설정 
+	  if(score >= 60) { // 조건 설정 
+		  System.out.println("합격입니다");
+	  }else if(score >=40) {
+		  System.out.println("불합격입니다");
+	  }else {
+		  System.out.println("탈락입니다.");
+	  }
+	  scanner.close();
+  } 
+  /////////////////////////////////////////////////////////////////////
+  private static void ifElse() {
+	  Scanner scanner = new Scanner(System.in);
+	  System.out.println("정수를 입력하세요");
+	  int num = scanner.nextInt();
+	  if(num >0) {
+		  System.out.println("양수 입니다");
+	  } else if (num <0) {
+		  System.out.println("음수 입니다");
+	  } else { 
+		  System.out.println("0 입니다");
+	  }
+	  scanner.close();
+  }
+///////////////////////////////////////////////////////////////////////////////////////////
+  private static void ifif() {
+	  Scanner scanner = new Scanner(System.in);
+	  System.out.println("정수를 입력하세요");
+	  int num = scanner.nextInt();
+  
+ 
+	  if (num == 0){
+		  System.out.println("0입니다");
+	  }else {
+		  if(num> 0) { 
+			  System.out.println("양수 입니다");
+		  } else {
+			  System.out.println("0 입니다");
+
+		  }
+	  }
+	  scanner.close(); // if문의 중첩이 3단계 이상이라면 의심을 해봐야함. 2단계 이하로 줄일 수 있음.
+}
+  ////////////////////////////////////////////////////////////////////////////////////////
+  private static void ifPractice() {
+	  
+	  
+	 Scanner scanner = new Scanner (System.in);
+	 System.out.println("과목을 선택하세요 " + "\n(1.자바 2.c 3.c++ 4.파이썬)\n" );
+	 int classroom = scanner.nextInt();
+	 System.out.println( "과목번호"+ classroom);
+	 if (classroom ==1) {
+		 System.out.println("R"+ classroom + "0"+ classroom +"호 입니다");
+	 } else if ( classroom ==2) {
+		 System.out.println("R"+ classroom + "0"+ classroom +"호 입니다");
+	 } else if ( classroom ==3) {
+		 System.out.println("R"+ classroom + "0"+ classroom +"호 입니다");
+ 	 } else if ( classroom ==4) {
+		 System.out.println("R"+ classroom + "0"+ classroom +"호 입니다");
+ 	 } else {
+		 System.out.println("나머지는 상담원에게 문의하세요."); // 조건이 ==로 표현될 때 switch case 사용 
+		
+
+ 	 } scanner.close();
+  }
+  //////////////////////////////////////////////////////////////////////////////////////////
+  private static void  switchCase(){
+	  
+	  Scanner scanner = new Scanner (System.in);
+		 System.out.println("과목을 선택하세요 " + "\n(1.자바 2.c 3.c++ 4.파이썬)\n" );
+		 int classroom = scanner.nextInt();
+		 switch(classroom){
+		 case 1 : 
+			 System.out.println("R"+ classroom + "0"+ classroom +"호 입니다");
+             break;
+		 case 2 : 
+			 System.out.println("R"+ classroom + "0"+ classroom +"호 입니다");
+             break;
+		 case 3 : 
+			 System.out.println("R"+ classroom + "0"+ classroom +"호 입니다");
+             break;
+		 case 4 : 
+			 System.out.println("R"+ classroom + "0"+ classroom +"호 입니다");
+             break;
+             
+            default :
+            	System.out.println("상담원에게 문의하세요");
+            	break;
+		 }
+  
+  
+  scanner.close();
+  
+  }
+  //////////////////////////////////////////////////////////////////////////////////////////
+  
+	 /*Scanner scanner = new Scanner (System.in);
+	 System.out.println("과목을 선택하세요 " + "\n(1.자바 2.c 3.c++ 4.파이썬)\n" );
+	 int classroom = scanner.nextInt();
+	 System.out.println( "과목번호"+ classroom);
+	  switch (classroom){
+	  case1 classroom == 1 :System.out.println("R"+ classroom + "0"+ classroom +"호 입니다");
+	  break;
+	  case classroom == 2 :System.out.println("R"+ classroom + "0"+ classroom +"호 입니다");
+	  break;
+	  case classroom == 3 :System.out.println("R"+ classroom + "0"+ classroom +"호 입니다");
+	  break;
+	  case classroom == 4 :System.out.println("R"+ classroom + "0"+ classroom +"호 입니다");
+	  break;
+	  default : 
+	  System.out.println("상담원에게 문의하세요");
+	  break}
+
+	 
+	 
+	  scanner.close();
+  //////////////////////////////////////////////////////////////////////
+  /* 강사님이 풀어주신 코드임. print 와 println은 뭔 차이일까 ?
+   * Scanner scanner = new Scanner (System.in);
+   * System.out.print("과목코드(1.c,2.c++,3.java,4.python)");
+   * int code = scanner.nextInt();
+   * 
+   * if (code ==1){
+   * System.out.println("R101입니다");
+   * } else if ( code ==2 ){
+   * System.out.println("R202입니다");
+   * } else if (code ==3){
+   * System.out.println("R303입니다");
+   * } else if (code ==4) {
+   * System.out.println("R404입니다");
+   * }else { 
+   * System.out.println("상담원에게 문의하세요");
+   * }
+   * scanner.close();
+   * }
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   */
+  
+  private static void switchEx2() {
+	  
+	  Scanner scanner = new Scanner(System.in);
+	  System.out.print("월을 입력하세요");
+	  int month = scanner.nextInt();
+	  
+	  switch( month) {
+	  case 1:
+	  case 3:
+	  case 5:
+	  case 7:
+	  case 8:
+	  case 10:
+	  case 12:
+		  System.out.println(month + "월은 31일 입니다");
+		  
+		  break;
+		  
+	  case 2:
+		  System.out.println(month + "월은 28일 입니다");
+		  break;
+		  
+	  case 4:
+	  case 6:
+	  case 9:
+	  case 11:
+		  System.out.println(month + "월은 30일입니다");
+		  break;
+	default:
+		System.out.println("?");
+	break;
+	 
+	  }
+	  scanner.close();
+  }
+  
+  
+  private static void switchEx3(){
+	
+	  String day = "TUE";
+	  String message;
+	  // 요일변수에는 지정된 값만 들어와야함, 문자열 사용은 자제하자 enum 데이터타입으로 변경하는게 좋음.
+	  switch(day) {
+	  case "SUN" :
+		  message ="일요일";
+		  break;
+		  
+	  case "MON":
+	  case "TUE":
+	  case "WED":
+	  case "THU":
+		  message ="열공";
+		  break;
+		  
+	  case  "FRI":
+			  message = "불금";
+			  break;
+	  case "SAT" :
+		  message = "주말";
+		  default:
+			  message ="?";
+			  break;
+		 
+	 
+  }
+	  
+ System.out.println(day+"에는"+message);
+  }
+ 
+ private static void aaa() {
+	 
+	 Scanner scanner = new Scanner(System.in);
+	 System.out.print("점수를 입력하세용");
+	 int score = scanner.nextInt();
+	 /*
+	 if (score%3 ==0) {
+		 System.out.println(score +"은 3의 배수 입니다.");
+	 }else {
+		 System.out.println(score +"은 3의 배수가 아닙니다.");
+	 }
+	 */
+	 
+	 System.out.println(score +"는"+ (score%3 == 0? "3의배수 입니다": "3의배수가 아닙니다"));// 이 조건문 엄청 좋다 기억하자. (조건 ? yes : no)
+	 scanner.close();
+
+ 
+ 
+ 
+  }
+  
+  /*
+  private static void ifEx4() {
+	  
+	  Scanner scanner = new Scanner(System.in);
+	  System.out.print("점수를 입력하세요");
+	  int score = scanner.nextInt();
+	  if (score >=90) {
+		  System.out.println("A등급");
+	  }else if (score >= 80) {
+		  System.out.println("B등급");
+	  }else if (score >= 70) {
+		  System.out.println("C등급");
+	  }else if (score >=60) {
+		  System.out.println("D등급");
+	  }else {
+		  System.out.println("F등급");
+	  }
+	  scanner.close();
+  }
+  
+  */
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
--- javaeex/src/com/javaex/basic/ConsoleEx.java
+++ javaeex/src/com/javaex/basic/ConsoleEx.java
@@ -1,15 +1,48 @@
 package com.javaex.basic;
 
+
+
+// 클래스 임포트
+import java.util.Scanner;
 public class ConsoleEx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     consoleOutEx();
+     //consoleOutEx();
+     consoleInputEx();
 	}
-    //콘손출력 ?
+	//콘솔 입력
+	private static void consoleInputEx() {
+		
+		Scanner scanner = new Scanner(System.in); // 표준 입력방식(키보드)
+		System.out.println("이름은 ?");
+		String name = scanner.next(); // 문자열 입력 받기
+		System.out.println("나이는 ?");
+		int age = scanner.nextInt(); // 정수값 입력 받기 
+		System.out.println("성별은 ?");
+		String haha = scanner.next();
+		
+
+		System.out.println("당신의 이름은" + name +"이고 나이는" + age + "이며 성별은 " + haha +"입니다");
+		
+		//scanner 라는 자원을 사용한 이후이므로 닫아줘야함. 다른 시스템이 이 자원을 사용하지 못하기 떄문 ?
+		
+		scanner.close();
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+    //콘손출력 
 	private static void consoleOutEx() {
 		//표준 출력 (stdout) : System.out
 		//표춘 에러 (stderr) : System.err
+		//표춘 입력 : System.in
+		
 		//메서드 print -> 출력후 개행안함
 		//메서드 println > 출력후 개행함 
 		//printf() > 포맷 출력 > string에서함 
@@ -28,6 +61,12 @@ public class ConsoleEx {
 		String filePath = "D:\\myfolder\\myfile.txt";
 		System.out.println(filePath);
 
+		// Scanner 클래스  Scanner scanner = new Scanner(System.in); 클래스가 스스로를 인스턴스 ? 
+		//import java.util.Scanner; (알트 쉬프트 o) 입력값은 대개 정수int  실수double 문자열string 단 앞에 next가 붙음 .
+		// 예를 들어 int nextInt(); , double next Double(); 이들을 입력소스라고 함.
+		// int value = scanner.nextInt(); 의 경우 다음에 입력되는 정수값을 value라는 메서드? 인스턴스에 입력.
 		
 	}
+	
+	
 }
