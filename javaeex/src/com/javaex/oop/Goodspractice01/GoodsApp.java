ed2ec441211664a5892f51fdcb6dd6c758ec2971
안지훈 <tw224@naver.com>
2021-03-04 19:47:42 +0900

    1

--- javaeex/src/com/javaex/oop/Goodspractice01/GoodsApp.java
+++ javaeex/src/com/javaex/oop/Goodspractice01/GoodsApp.java
@@ -7,17 +7,17 @@ public class GoodsApp {
 
 		Goods camera = new Goods();
 		camera.setName("니콘");
-		//camera.setPrice(50000);
+		camera.setPrice(50000);
 		camera.showInfo();
 		
 		Goods nb = new Goods();
 		nb.setName("LG Gram");
-		//nb.setPrice(1000000000);
+		nb.setPrice(1000000000);
 		nb.showInfo();
 		
 		Goods cup = new Goods();
 		cup.setName("물");
-		//cup.setPrice(500);
+		cup.setPrice(500);
 		cup.showInfo();
 		
 		
